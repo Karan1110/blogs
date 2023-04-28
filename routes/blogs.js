b324f998dc7e91c5baa93d9a03a368eceab9dbf4
KaranCS14 <hamsithacskg2ipsvig@gmail.com>
2023-04-28 14:03:09 +0530

    add CRUD features for blogs

---
+++ routes/blogs.js
@@ -0,0 +1,103 @@
+const express = require("express")
+const router = express.Router();
+const withDBConnection = require("../middlewares/connectDB");
+
+router.get('/blogs', async (req, res) => {
+    const { rows } = await req.db.query(
+        `
+        SELECT * FROM Blogs 
+        
+        `, []
+    )
+    res
+    .status(200)
+    .send(rows);
+});
+
+
+router.get('/blogs/:id', async (req, res) => {
+    const { rows } = await req.db.query(
+        `
+        SELECT * FROM Blogs WHERE id = $1 
+        
+        `, [req.params.id]
+    )
+    res
+    .status(200)
+    .send(rows[0]);
+});
+
+router.get('/blogs/pagination/', async (req, res) => {
+    const { rows } = await req.db.query(
+        `
+        SELECT * FROM Blogs LIMIT $1 OFFSET $2 
+        
+        `, [req.query.limit,req.query.offset]
+    )
+    res
+    .status(200)
+    .send(rows);
+});
+
+router.get('/blogs/pagination/', async (req, res) => {
+    const { rows } = await req.db.query(
+        `
+        SELECT * FROM Blogs LIMIT $1 OFFSET $2 
+        
+        `, [req.query.limit,req.query.offset]
+    )
+    res
+    .status(200)
+    .send(rows);
+});
+
+router.post('/blogs/post', async (req, res) => {
+    const { rows } = await req.db.query(
+        `
+       INSERT INTO Blogs(title,author,createdAt)
+       VALUES($1,$2,$3) 
+       RETURNING *
+        
+        `, [req.body.title,req.body.author,req.body.createdAt]
+    )
+    res
+    .status(201)
+    .send(rows[0]);
+});
+
+
+router.put('/blogs/update', async (req, res) => {
+    const { rows } = await req.db.query(
+        `
+      UPDATE Blogs
+      SET title=$1,
+          author=$2,
+          updatedAt=$3
+      WHERE id = $4
+
+    RETURNING *
+        
+        
+        `, [req.body.title,req.body.author,Date.now(),req.params.id]
+    )
+    res
+    .status(200)
+    .send(rows[0]);
+});
+
+
+
+router.delete('/blogs/delete/:id', async (req, res) => {
+    const { rows } = await req.db.query(
+        `
+      DELETE FROM Blogs
+      WHERE id = $1
+        
+        `, [req.params.id]
+    )
+    res
+    .status(200)
+    .send(rows[0]);
+});
+
+module.exports = router;
